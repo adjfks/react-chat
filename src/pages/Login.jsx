03726b4e5ea5513f838c50c47b81941b00360b71
corner <sptdfdzl@163.com>
2023-04-04 11:25:15 +0800

    style: page login & register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="form-container">
+      <div className="form-wrapper">
+        <span className="logo">Corner Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign In</button>
+        </form>
+        <p>You don't have an account? Sign Up</p>
+      </div>
+    </div>
   )
 }
 
