5fc3d2a5bb843ae6f3428801a257799c7a39f2ba
corner <sptdfdzl@163.com>
2023-04-08 12:06:47 +0800

    style: ui done

---
+++ src/components/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='message owner'>
+      <div className="message-info">
+        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <span>just now</span>
+      </div>
+      <div className="message-content">
+        <p>Hello</p>
+        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Message
