93a859cb014e5423cbfd066f62446e4428c57ff8
corner <sptdfdzl@163.com>
2023-04-10 13:40:16 +0800

    feat: chat list done

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { db } from '../firebase'
 
 const Chats = () => {
+  const [chats, setChats] = useState([])
+  const { currentUser } = useContext(AuthContext)
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, 'userChats', currentUser.uid), (doc) => {
+        setChats(
+          Object.entries(doc.data()).map((chat) => ({
+            id: chat[0],
+            userInfo: chat[1].userInfo,
+            date: chat[1].date,
+          }))
+        )
+      })
+    }
+    currentUser.uid && getChats()
+  }, [currentUser.uid])
   return (
-    <div className='chats'>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="user-chat">
-        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-        <div className="user-chat-info">
-          <span>Jane</span>
-          <p>Hello</p>
-        </div>
-      </div>
+    <div className="chats">
+      {chats.map((chat) => {
+        return (
+          <div className="user-chat" key={chat.id}>
+            <img src={chat.userInfo.photoURL} alt="" />
+            <div className="user-chat-info">
+              <span>{chat.userInfo.displayName}</span>
+              <p>{chat.userInfo.lastMessage?.text || ''}</p>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
